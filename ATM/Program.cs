868560e55df68b873f802267dc764d0e55c3dd5b
DayhanaRamirez <dayhanaramirez45@gmail.com>
2022-03-25 13:40:58 -0500

    Added another user

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -7,18 +7,32 @@ namespace ATM
     {
         static void Main(string[] args)
         {
-            SavingsAccount accountA = new SavingsAccount()
+            SavingsAccount account1 = new SavingsAccount()
             {
                 Id = 1,
                 Name = "Cuenta de Ahorros",
                 Balance = 500000,
             };
 
-            CIFAccount accountB = new CIFAccount()
+            SavingsAccount account3 = new SavingsAccount()
+            {
+                Id = 3,
+                Name = "Cuenta de Ahorros",
+                Balance = 2000000,
+            };
+
+            CIFAccount account2 = new CIFAccount()
             {
                 Id = 2,
                 Name = "Fiducuenta",
                 Balance = 1000000,
+            };
+
+            CIFAccount account4 = new CIFAccount()
+            {
+                Id = 4,
+                Name = "Fiducuenta",
+                Balance = 3000000,
             };
 
             User admin = new User()
@@ -27,20 +41,26 @@ namespace ATM
                 LastName = "Ramirez",
                 UserName = "DayhanaR",
                 Password = "0000",
-                Accounts = new List<Account>() { accountA, accountB }   
-            };    
+                Accounts = new List<Account>() { account1, account2 }   
+            };
+
+
+            User user1 = new User()
+            {
+                Name = "Angela",
+                LastName = "Moreno",
+                UserName = "AngelaM",
+                Password = "0000",
+                Accounts = new List<Account>() { account3, account4 }
+            };
 
             Bank bancolombia = new Bank()
             {
-                Users = new List<User>() { admin }
-
+                Users = new List<User>() { admin, user1 }
             };
 
             ATM cajero1 = new ATM();
-
             cajero1.Login(bancolombia);
-
-   
         }
     }
 }
